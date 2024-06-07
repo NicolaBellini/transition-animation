b09eb0f633d0346817f76c3a44729188b644af38
NicolaBellini <bellininicola4366@gmail.com>
2024-06-07 21:51:27 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import headerVue from "./components/header.vue";
+import mainVue from "./components/main.vue";
+
+export default {
+  components: {
+    headerVue,
+    mainVue,
+  },
+};
+</script>
+
+<template>
+  <div class="d-flex flex-column">
+    <headerVue />
+    <mainVue />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "./assets/scss/main.scss";
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,52 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="box-wrapper d-flex flex-column align-items-center">
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+    <div class="box d-flex justify-content-center align-items-center m-3">
+      box
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  height: 100px;
+  width: 100px;
+  background-color: red;
+}
+</style>
