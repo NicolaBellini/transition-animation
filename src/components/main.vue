d25d1767a39a919e6a39b6ab1c3b595a7472a7bf
NicolaBellini <bellininicola4366@gmail.com>
2024-06-07 22:02:27 +0200

    [add] animation with js function()

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,5 +1,19 @@
 <script>
 export default {};
+document.addEventListener("DOMContentLoaded", () => {
+  const boxes = document.querySelectorAll(".box");
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else if (!entry.isIntersecting) {
+        entry.target.classList.remove("visible");
+      }
+    });
+  });
+
+  boxes.forEach((box) => observer.observe(box));
+});
 </script>
 
 <template>
@@ -44,9 +58,27 @@ export default {};
 </template>
 
 <style scoped lang="scss">
+@keyframes appear {
+  from {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
 .box {
   height: 100px;
   width: 100px;
   background-color: red;
+  opacity: 0; /* Start hidden */
+  transform: scale(0.5); /* Start scaled down */
+  transition: opacity 0.5s, transform 0.5s; /* Smooth transition for appearance */
+}
+.box.visible {
+  opacity: 1; /* Become visible */
+  transform: scale(1); /* Return to original size */
+  animation: appear 0.5s linear; /* Apply the animation */
 }
 </style>
